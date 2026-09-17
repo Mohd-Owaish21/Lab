9168c195306dd05df241909794d8b370eb2891ef
Mohd Owaish <mohammad.owaish0021@gmail.com>
2026-09-17 16:01:08 +0530

    Update Q4.cpp

--- Week 6/Q4.cpp
+++ Week 6/Q4.cpp
@@ -1,17 +1,3 @@
-//#include<iostream>
-//using namespace std;
-//int main()
-//{
-//	int a,b;
-//	cout<<"Enter two number: ";
-//	cin>>a>>b;
-//	int *ptr1=&a;
-//	int *ptr2=&b;
-//	cout<<"a: "<<a<<" "<<"b: "<<b<<endl;
-//	cout<<"a: "<<(*ptr1)+1<<" "<<"b: "<<(*ptr2)+1<<endl;
-//	return 0;
-//}
-
 #include<iostream>
 using namespace std;
 int main()
